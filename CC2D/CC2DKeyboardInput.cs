9cdd0249d3b92325299d9b6a48a7ae157786b9ca
留白 <156890093@qq.com>
2018-05-25 03:25:15 +0800

    cc2d

---
+++ CC2D/CC2DKeyboardInput.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace UnityStandardUtils.CC2D
+{
+    [RequireComponent(typeof(CharacterController2D))]
+    public class CC2DKeyboardInput : MonoBehaviour
+    {
+        /// <summary>
+        /// 键位设置
+        /// </summary>
+        [System.Serializable]
+        public class _KeySetting
+        {
+            public KeyCode Left = KeyCode.A;
+            public KeyCode Right = KeyCode.D;
+
+            public KeyCode Sprint = KeyCode.LeftShift;
+            public KeyCode Crouch = KeyCode.LeftControl;
+
+            public KeyCode ClimbUp = KeyCode.W;
+            public KeyCode ClimbDown = KeyCode.S;
+
+            public KeyCode Jump = KeyCode.Space;
+
+            public KeyCode Interact = KeyCode.E;
+        }
+
+        [Tooltip("将对应按键与当前物品的CharactorController2D的输入器进行绑定")]
+        public _KeySetting KeySetting;
+
+        private CharacterController2D cc2d;
+
+
+        private void readKeyboardInput()
+        {
+            cc2d.InputStatus.Left = Input.GetKey(KeySetting.Left);
+            cc2d.InputStatus.Right = Input.GetKey(KeySetting.Right);
+
+            cc2d.InputStatus.Sprint = Input.GetKey(KeySetting.Sprint);
+            cc2d.InputStatus.Crouch = Input.GetKey(KeySetting.Crouch);
+
+            cc2d.InputStatus.ClimbUp = Input.GetKey(KeySetting.ClimbUp);
+            cc2d.InputStatus.ClimbDown = Input.GetKey(KeySetting.ClimbDown);
+
+            cc2d.InputStatus.Jump = Input.GetKeyDown(KeySetting.Jump);
+            cc2d.InputStatus.Interact = Input.GetKeyDown(KeySetting.Interact);
+        }
+
+        private void Start()
+        {
+            cc2d = GetComponent<CharacterController2D>();
+        }
+        private void Update()
+        {
+            readKeyboardInput();
+        }
+    }
+
+}
---
+++ GizmosTool.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace UnityStandardUtils
+{
+    public class GizmosTool
+    {
+
+        public static void Text(GUISkin guiSkin, string text, Vector3 position, Color? color = null, int fontSize = 0, float yOffset = 0)
+        {
+            //#if UNITY_EDITOR
+            var prevSkin = GUI.skin;
+            if (guiSkin == null)
+                Debug.LogWarning("editor warning: guiSkin parameter is null");
+            else
+                GUI.skin = guiSkin;
+
+            GUIContent textContent = new GUIContent(text);
+
+            GUIStyle style = (guiSkin != null) ? new GUIStyle(guiSkin.GetStyle("Label")) : new GUIStyle();
+            if (color != null)
+                style.normal.textColor = (Color)color;
+            if (fontSize > 0)
+                style.fontSize = fontSize;
+
+            Vector2 textSize = style.CalcSize(textContent);
+            Vector3 screenPoint = Camera.current.WorldToScreenPoint(position);
+
+            if (screenPoint.z > 0) // checks necessary to the text is not visible when the camera is pointed in the opposite direction relative to the object
+            {
+                var worldPosition = Camera.current.ScreenToWorldPoint(new Vector3(screenPoint.x - textSize.x * 0.5f, screenPoint.y + textSize.y * 0.5f + yOffset, screenPoint.z));
+                UnityEditor.Handles.Label(worldPosition, textContent, style);
+            }
+            GUI.skin = prevSkin;
+            //#endif
+        }
+        public static void Circle(float r,Vector3 center, Color? color=null)
+        {
+            Gizmos.color = color==null?Color.white:(Color)color;
+
+            Vector3 beginPoint = Vector3.zero;
+            Vector3 firstPoint = Vector3.zero;
+            for (float theta = 0; theta < 2 * Mathf.PI; theta += 0.1f)
+            {
+                float x = r * Mathf.Cos(theta) + center.x;
+                float y = r * Mathf.Sin(theta) + center.y;
+                Vector3 endPoint = new Vector3(x, y, 0);
+                if (theta == 0)
+                    firstPoint = endPoint;
+                else Gizmos.DrawLine(beginPoint, endPoint);
+                beginPoint = endPoint;
+            }
+
+            Gizmos.DrawLine(firstPoint, beginPoint);
+        }
+        public static void Capsule2D(Vector3 center,Vector2 size,Vector3 scale, Color? color = null)
+        {
+            Gizmos.color = color == null ? Color.white : (Color)color;
+
+            float hx = size.x / 2f;
+            float hy = size.y / 2f - size.x / 2f;
+            if (hx < 0f) hx = 0f;
+            if (hy < 0f) hy = 0f;
+            hx *= Mathf.Abs(scale.x);
+            hy *= Mathf.Abs(scale.y);
+
+            Vector3 RUp = new Vector3(center.x + hx, center.y + hy, center.z);
+            Vector3 RLow = new Vector3(center.x + hx, center.y - hy, center.z);
+
+            Vector3 LLow = new Vector3(center.x - hx, center.y - hy, center.z);
+            Vector3 LUp = new Vector3(center.x - hx, center.y + hy, center.z);
+
+            //Right Line
+            Gizmos.DrawLine(RUp, RLow);
+            //Left Line
+            Gizmos.DrawLine(LLow, LUp);
+
+
+            //Upper Half Circle
+            Vector3 beginPoint = Vector3.zero;
+            for (float theta = 0; theta < Mathf.PI; theta += 0.1f)
+            {
+                float x = hx * Mathf.Cos(theta) + center.x;
+                float y = hx * Mathf.Sin(theta) + center.y + hy;
+                Vector3 endPoint = new Vector3(x, y, center.z);
+                if(theta != 0) Gizmos.DrawLine(beginPoint, endPoint);
+                beginPoint = endPoint;
+            }
+            Gizmos.DrawLine(LUp, beginPoint);
+
+            //Lower Half Circle
+            beginPoint = RLow;
+            for (float theta = Mathf.PI; theta < 2 * Mathf.PI; theta += 0.1f)
+            {
+                float x = hx * Mathf.Cos(theta) + center.x;
+                float y = hx * Mathf.Sin(theta) + center.y - hy;
+                Vector3 endPoint = new Vector3(x, y, center.z);
+                if (theta != Mathf.PI) Gizmos.DrawLine(beginPoint, endPoint);
+                beginPoint = endPoint;
+            }
+            Gizmos.DrawLine(RLow, beginPoint);
+        }
+
+    }
+}

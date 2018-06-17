fcea8843971f94da9990a39202edbb723b55360c
留白 <156890093@qq.com>
2018-06-17 18:40:28 +0800

    prefabfield

---
+++ UnityStandardUtils/PrefabField.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace UnityStandardUtils
+{
+    [System.Serializable]
+    public class PrefabField
+    {
+        [SerializeField]
+        private string prefabPath = string.Empty;
+
+        public string PrefabPath
+        {
+            get
+            {
+                return prefabPath;
+            }
+        }
+
+        public static implicit operator string(PrefabField p)
+        {
+            return p.prefabPath;
+        }
+
+        public void SetPath(string s)
+        {
+            prefabPath = s;
+        }
+    }
+
+}
---
+++ UnityStandardUtilsEditor/PrefabFieldPropertyDrawer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityStandardUtils;
+
+namespace UnityStandardUtilsEditor
+{
+
+    [CustomPropertyDrawer(typeof(PrefabField))]
+    public class PrefabFieldPropertyDrawer : PropertyDrawer
+    {
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, GUIContent.none, property);
+
+            var prefabPath = property.FindPropertyRelative("prefabPath");
+
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            EditorGUI.BeginChangeCheck();
+
+            var value = EditorExtension.GetPrefabPath((GameObject)EditorGUI.ObjectField(position, EditorExtension.LoadPrefabPath(prefabPath.stringValue), typeof(GameObject), false));
+
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                prefabPath.stringValue = value;
+            }
+
+            EditorGUI.EndProperty();
+        }
+
+
+
+    }
+
+}

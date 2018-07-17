785fe2a9180145128f50f9097d5828b5740e814c
留白 <156890093@qq.com>
2018-07-17 21:41:32 +0800

    ADD

---
+++ UnityStandardUtils/PauseManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityStandardUtils.Extension;
+
+namespace UnityStandardUtils
+{
+    public class PauseManager:SingletonMonoBehaviour<PauseManager>
+    {
+
+        private bool IsPause()
+        {
+            return Time.timeScale == 0f;
+        }
+
+        private void Pause()
+        {
+            Time.timeScale = 0f;
+        }
+        private void Resume()
+        {
+            Time.timeScale = 1f;
+        }
+
+        public static void Pause(bool? Status = null)
+        {
+            if (Status == null)
+            {
+                if (Instance.IsPause()) Instance.Resume(); 
+                else Instance.Pause();
+
+                return;
+            }
+
+            if (Status == true) Instance.Pause();
+            if (Status == false) Instance.Resume();
+        }
+
+        public static bool IsPaused
+        {
+            get { return Instance.IsPause(); }
+        }
+    }
+}
